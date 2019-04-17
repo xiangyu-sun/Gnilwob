52ec33daf23253a4e9de75868472f9ad35f5dfe0
Xiangyu Sun <xiangyu.sun@dnata.com>
2019-04-17 19:07:32 +0400

    Initial Commit

---
+++ Bowlingure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bowlingure
+//
+//  Created by 孙翔宇 on 4/17/19.
+//  Copyright © 2019 孙翔宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BowlingureTests/BowlingureTests.swift
@@ -0,0 +1,34 @@
+//
+//  BowlingureTests.swift
+//  BowlingureTests
+//
+//  Created by 孙翔宇 on 4/17/19.
+//  Copyright © 2019 孙翔宇. All rights reserved.
+//
+
+import XCTest
+@testable import Bowlingure
+
+class BowlingureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
